08f3dfdb680726fb26a3988d2a37f03d1e112e12
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-09 10:14:04 -0300

    Finishing responsive design

--- Ejercicios/js/main.js
+++ Ejercicios/js/main.js
@@ -1,11 +1,32 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        console.log('funca')
-        var menu = $('#menu');
-        if (menu.offset().top > 56) {
-            menu.addClass("bg-inverse");
-        } else {
-            menu.removeClass("bg-inverse");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    console.log("funca");
+    var menu = $("#menu");
+    if (menu.offset().top > 56 && $(window).width() > 963) {
+      menu.addClass("bg-inverse");
+    } else {
+      menu.removeClass("bg-inverse");
+    }
+
+    if ($(window).width() < 963) {
+      menu.addClass("bg-dark");
+      menu.addClass("nav-margin");
+    } else {
+      menu.removeClass("nav-margin");
+    }
+  });
+  $(document).ready(function () {});
+
+  console.log($(window).width());
+
+  var img = $("img.slideUp");
+
+  //   img.hover(function (e) {
+  //     img
+  //       .animate({ marginTop: "200px", position: "relative" }, 300)
+  //       .animate({ marginTop: "0px" }, 300);
+  //   });
+  //   $(window).load(function () {
+  //     $("#slide-up").slideUp();
+  //   });
 });
