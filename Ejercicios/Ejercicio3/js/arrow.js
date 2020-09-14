1c9aa86cefb427340740309145b9810d9667636a
Maciel <macielgaleano.jh@gmail.com>
2020-09-14 03:24:10 -0300

    Finished

--- Ejercicios/Ejercicio3/js/arrow.js
+++ Ejercicios/Ejercicio3/js/arrow.js
@@ -2,6 +2,6 @@ $(".nav-link").on("click", function () {
   $(".fa-arrow-right", this).css("transform", "rotate(90deg)");
 });
 
-$("dropdown").on("hidden.bs.dropdown", function () {
+$(".dropdown").on("hidden.bs.dropdown", function () {
   $(".fa-arrow-right").css("transform", "rotate:0deg");
 });
