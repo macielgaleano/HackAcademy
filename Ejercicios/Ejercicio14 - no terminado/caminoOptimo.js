5afb457a3b74e68172ab62319b6ca650c371cd8a
Maciel <macielgaleano.jh@gmail.com>
2020-09-15 11:34:02 -0300

    nuevoCamino finished

--- Ejercicios/Ejercicio14 - no terminado/caminoOptimo.js
+++ Ejercicios/Ejercicio14 - no terminado/caminoOptimo.js
@@ -1,30 +1,52 @@
-function caminoOptimo(ruta) {
-  for (let index = 0; index < ruta.length; index + 2) {
-    var indexValue = index;
-    var arrayDevuelto = [];
-    var actual = ruta[indexValue];
-    var siguiente = ruta[indexValue + 1];
-    console.log(indexValue, actual, siguiente);
-    if (actual == siguiente) {
-      arrayDevuelto.push(actual);
-      arrayDevuelto.push(siguiente);
-    } else if (
-      (actual == "ESTE" && siguiente == "OESTE") ||
-      (siguiente == "ESTE" && actual == "OESTE")
+function caminoOptimo(camino) {
+  if (camino.lenght <= 1) return camino;
+
+  var nuevoCamino = [];
+  nuevoCamino[0] = camino[0];
+
+  for (let i = 0; i < camino.length; i++) {
+    var ultimoElemenNuevoCamino = nuevoCamino[nuevoCamino.length - 1];
+
+    if (
+      (ultimoElemenNuevoCamino === "SUR" && camino[i] === "NORTE") ||
+      (ultimoElemenNuevoCamino === "NORTE" && camino[i] === "SUR") ||
+      (ultimoElemenNuevoCamino === "OESTE" && camino[i] === "ESTE") ||
+      (ultimoElemenNuevoCamino === "ESTE" && camino[i] === "OESTE")
     ) {
-      break;
-    } else if (
-      (actual == "SUR" && siguiente == "NORTE") ||
-      (siguiente == "NORTE" && actual == "SUR")
-    ) {
-      break;
+      nuevoCamino.pop();
     } else {
-      arrayDevuelto.push(actual);
-      arrayDevuelto.push(siguiente);
-      console.log(arrayDevuelto);
-      break;
+      nuevoCamino.push(camino[i]);
     }
   }
-  return arrayDevuelto;
 }
+
+// function caminoOptimo(ruta) {
+//   for (let index = 0; index < ruta.length; index + 2) {
+//     var indexValue = index;
+//     var arrayDevuelto = [];
+//     var actual = ruta[indexValue];
+//     var siguiente = ruta[indexValue + 1];
+//     console.log(indexValue, actual, siguiente);
+//     if (actual == siguiente) {
+//       arrayDevuelto.push(actual);
+//       arrayDevuelto.push(siguiente);
+//     } else if (
+//       (actual == "ESTE" && siguiente == "OESTE") ||
+//       (siguiente == "ESTE" && actual == "OESTE")
+//     ) {
+//       break;
+//     } else if (
+//       (actual == "SUR" && siguiente == "NORTE") ||
+//       (siguiente == "NORTE" && actual == "SUR")
+//     ) {
+//       break;
+//     } else {
+//       arrayDevuelto.push(actual);
+//       arrayDevuelto.push(siguiente);
+//       console.log(arrayDevuelto);
+//       break;
+//     }
+//   }
+//   return arrayDevuelto;
+// }
 // caminoOptimo(["NORTE", "OESTE", "SUR", "ESTE"]);
