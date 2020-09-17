910f4c8c61f7cff7e1c199bccae2d8f3eac076e3
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-17 09:40:08 -0300

    Rename invalid direct

--- Ejercicios/Ejercicio3/js/arrow.js
+++
@@ -1,7 +0,0 @@
-$(".nav-link").on("click", function () {
-  $(".fa-arrow-right", this).css("transform", "rotate(90deg)");
-});
-
-$(".dropdown").on("hidden.bs.dropdown", function () {
-  $(".fa-arrow-right").css("transform", "rotate:0deg");
-});
